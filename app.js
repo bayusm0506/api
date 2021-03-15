b00d9a1a6c91b928434c6aacbc6c5e91769004e0
bayusm0506 <bayumaulana472@gmail.com>
2021-03-15 08:17:45 +0700

    Test Running

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+// const port = 3000;
 
 app.use(bodyParser.json());
 app.use(
@@ -14,6 +14,8 @@ app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
-});
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}.`);
+// });
+
+module.exports = app;
