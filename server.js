62e3528979b5a52954da4257ded2af09995bd3b0
bayusm0506 <bayumaulana472@gmail.com>
2021-03-19 19:26:13 +0700

    Delete files

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+var port = normalizePort(process.env.PORT || "3000");
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+module.exports = app;
