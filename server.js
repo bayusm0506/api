61652d34016f9ac2d8aff85d4925b712eb0cc019
bayusm0506 <bayumaulana472@gmail.com>
2021-03-19 19:38:26 +0700

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-var port = "3000";
+var port = 3000;
 
 app.use(bodyParser.json());
 app.use(
